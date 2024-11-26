691f4e98e4c549d30395ff2028b35c7794a20f20
StarKnightt <prasenjitt4e@gmail.com>
2024-11-26 21:49:49 +0530

    done almost everything

---
+++ src/data/resume.tsx
@@ -0,0 +1,321 @@
+import { Icons } from "@/components/icons";
+import { HomeIcon, NotebookIcon } from "lucide-react";
+
+export const DATA = {
+  name: "Prasenjit",
+  initials: "DV",
+  url: "https://github.com/StarKnightt",
+  location: "Odisha, India",
+  locationLink: "https://www.google.com/maps/place/odisha",
+  description:
+    "I'm a developer from India who enjoys creating and building things that help people. I'm also quite active on Twitter!",
+  summary:
+    "I Completed my B.tech Degree in [Computer Science and Information Technology](https://tat.ac.in/) at the end of 2024. Right Now, I'm learning Next.js and working on some [projects](https://github.com/StarKnightt) to sharpen my skills.",
+  avatarUrl: "/me.png",
+  skills: [
+    "React",
+    "Next.js",
+    "Typescript",
+    "Node.js",
+    "Java",
+    "MongoDB",
+    "JavaScript",
+    "git",
+    "Linux",
+  ],
+  navbar: [
+    { href: "/", icon: HomeIcon, label: "Home" },
+    { href: "/blog", icon: NotebookIcon, label: "Blog" },
+  ],
+  contact: {
+    email: "prasenjitt4e@gmail.com",
+    tel: "+91 6294925956",
+    social: {
+      GitHub: {
+        name: "GitHub",
+        url: "https://github.com/StarKnightt",
+        icon: Icons.github,
+
+        navbar: true,
+      },
+      LinkedIn: {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/prasenjitnayak/",
+        icon: Icons.linkedin,
+
+        navbar: true,
+      },
+      X: {
+        name: "X",
+        url: "https://x.com/StarKnightt",
+        icon: Icons.x,
+
+        navbar: true,
+      },
+      Youtube: {
+        name: "Youtube",
+        url: "https://youtube.com/@Star_Knight12",
+        icon: Icons.youtube,
+        navbar: true,
+      },
+      CodePen: {
+        name: "CodePen",
+        url: "https://codepen.io/StarKnightt",
+        icon: Icons.codepen,
+        navbar: true,
+      },
+      buyMeACoffee: {
+        name: "buyMeACoffee",
+        url: "https://buymeacoffee.com/prasen",
+        icon: Icons.buyMeACoffee,
+        navbar: true,
+      },
+      email: {
+        name: "Send Email",
+        url: "mailto:prasenjitt4e@gmail.com",
+        icon: Icons.email,
+        navbar: true,
+      },
+    },
+  },
+
+  work: [
+    {
+      company: "Atomic Finance",
+      href: "https://atomic.finance",
+      badges: [],
+      location: "Remote",
+      title: "Bitcoin Protocol Engineer",
+      logoUrl: "/atomic.png",
+      start: "May 2021",
+      end: "Oct 2022",
+      description:
+        "Implemented the Bitcoin discreet log contract (DLC) protocol specifications as an open source Typescript SDK. Dockerized all microservices and setup production kubernetes cluster. Architected a data lake using AWS S3 and Athena for historical backtesting of bitcoin trading strategies. Built a mobile app using react native and typescript.",
+    },
+    {
+      company: "Shopify",
+      badges: [],
+      href: "https://shopify.com",
+      location: "Remote",
+      title: "Software Engineer",
+      logoUrl: "/shopify.svg",
+      start: "January 2021",
+      end: "April 2021",
+      description:
+        "Implemented a custom Kubernetes controller in Go to automate the deployment of MySQL and ProxySQL custom resources in order to enable 2,000+ internal developers to instantly deploy their app databases to production. Wrote several scripts in Go to automate MySQL database failovers while maintaining master-slave replication topologies and keeping Zookeeper nodes consistent with changes.",
+    },
+    {
+      company: "Nvidia",
+      href: "https://nvidia.com/",
+      badges: [],
+      location: "Santa Clara, CA",
+      title: "Software Engineer",
+      logoUrl: "/nvidia.png",
+      start: "January 2020",
+      end: "April 2020",
+      description:
+        "Architected and wrote the entire MVP of the GeForce Now Cloud Gaming internal admin and A/B testing dashboard using React, Redux, TypeScript, and Python.",
+    },
+    {
+      company: "Splunk",
+      href: "https://splunk.com",
+      badges: [],
+      location: "San Jose, CA",
+      title: "Software Engineer",
+      logoUrl: "/splunk.svg",
+      start: "January 2019",
+      end: "April 2019",
+      description:
+        "Co-developed a prototype iOS app with another intern in Swift for the new Splunk Phantom security orchestration product (later publicly demoed and launched at .conf annual conference in Las Vegas). Implemented a realtime service for the iOS app in Django (Python) and C++; serialized data using protobufs transmitted over gRPC resulting in an approximate 500% increase in data throughput.",
+    },
+    {
+      company: "Lime",
+      href: "https://li.me/",
+      badges: [],
+      location: "San Francisco, CA",
+      title: "Software Engineer",
+      logoUrl: "/lime.svg",
+      start: "January 2018",
+      end: "April 2018",
+      description:
+        "Proposed and implemented an internal ruby API for sending/receiving commands to scooters over LTE networks. Developed a fully automated bike firmware update system to handle asynchronous firmware updates of over 100,000+ scooters worldwide, and provide progress reports in real-time using React, Ruby on Rails, PostgreSQL and AWS EC2 saving hundreds of developer hours.",
+    },
+    {
+      company: "Mitre Media",
+      href: "https://mitremedia.com/",
+      badges: [],
+      location: "Toronto, ON",
+      title: "Software Engineer",
+      logoUrl: "/mitremedia.png",
+      start: "May 2017",
+      end: "August 2017",
+      description:
+        "Designed and implemented a robust password encryption and browser cookie storage system in Ruby on Rails. Leveraged the Yahoo finance API to develop the dividend.com equity screener",
+    },
+  ],
+  education: [
+    {
+      school: "Trident Academy Of Technology",
+      href: "https://tat.ac.in/",
+      degree: "B.Tech in Computer Science and Information Technology",
+      logoUrl: "/buildspace.jpg",
+      start: "2020",
+      end: "2024",
+    },
+    {
+      school: "Netaji Subhas Memorial City College",
+      href: "https://www.nsmcity.ac.in/index.asp",
+      degree: "Higher Secondary",
+      logoUrl: "/waterloo.png",
+      start: "2016",
+      end: "2021",
+    },
+  ],
+  projects: [
+    {
+      title: "Resume Builder",
+      href: "https://resume-builder-ten-opal.vercel.app/",
+      dates: "May 2023 - Sept 2023",
+      active: true,
+      description:
+        "It was my final year [Project](https://github.com/StarKnightt/ResumeBuilder) in our college, It is a interactive and versatile Dynamic CV Builder, completely build from scratch with backend functionallity .",
+      technologies: [
+        "CSS",
+        "javascript",
+        "MongoDB",
+        "Express.js",
+        "HTML",
+        "Regex",
+        "Node.js",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://resume-builder-ten-opal.vercel.app/",
+          icon: <Icons.globe className="size-3" />,
+        },
+        {
+          type: "Source",
+          href: "https://github.com/StarKnightt/ResumeBuilder",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      
+      image: "",
+      video:
+        "https://video.gumlet.io/6745e593080b60408ca085f7/6745e5e5080b60408ca08984/download.mp4",
+    },
+    {
+      title: "Coffee-Website",
+      href: "https://coffee-websitee.vercel.app/",
+      dates: "September 2024 - October 2024",
+      active: true,
+      description:
+        "A web app, with the futurisitc yet nostalgic design of a coffee shop, with a menu and blend of retro vibes.",
+      technologies: [
+        "React.js",
+        "Git",
+        "TailwindCSS",
+        "Framer-motion",
+        "React-icons",
+        "React-router-dom",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://coffee-websitee.vercel.app/",
+          icon: <Icons.globe className="size-3" />,
+        },
+        {
+          type: "Source",
+          href: "https://github.com/StarKnightt/Coffee-Website",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: "",
+      video: "https://video.gumlet.io/6745e593080b60408ca085f7/6745ea2d080b60408ca0bc08/download.mp4",
+    },
+    {
+      title: "GitHub Buddy Finder",
+      href: "https://buddy-find.vercel.app/",
+      dates: "April 2023 - September 2023",
+      active: true,
+      description:
+        "This innovative web application helps developers connect with like-minded individuals based on their GitHub activity and language preferences.",
+      technologies: [
+        "React.js",
+        "Octokit",
+        "Rest API",
+        "TailwindCSS",
+        "react-icons",
+        "react-router-dom",
+        "Vite",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://buddy-find.vercel.app/",
+          icon: <Icons.globe className="size-3" />,
+        },
+        {
+          type: "Source",
+          href: "https://github.com/StarKnightt/Buddy-Finder",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: "",
+      video: "https://video.gumlet.io/6745e593080b60408ca085f7/6745ec82c84c6b7e105c3ee5/download.mp4",
+    },
+    {
+      title: "Solar System",
+      href: "https://solarrsystem.vercel.app/",
+      dates: "September 2024 - October 2024",
+      active: true,
+      description:
+        "This project is a visually stunning and interactive web application that provides information about the solar system and it's planet with music.",
+      technologies: [
+        "React.js",
+        "font-awesome",
+        "react-icons",
+        "react-dom",
+        "CSS3",
+        "Vite",
+        "Git",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://automatic.chat",
+          icon: <Icons.globe className="size-3" />,
+        },
+      ],
+      image: "",
+      video:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/automatic-chat.mp4",
+    },
+  ],
+  hackathons: [
+    {
+      title: "Smart India Hackathon",
+      dates: "March 23rd - 25th, 2022",
+      location: "Bhubaneswar, India",
+      description:
+        "Developed a learing portal with some free education content with the best paid contents too, the project was called as 'EducationX'.",
+      image:
+        "https://i.ibb.co/QDssrSq/smart-india-hackathon.jpg",
+      mlh: "https://github.com/Synchrotek/E-LearningX",
+      links: [],
+    },
+    {
+      title: "Smart India Hackathon",
+      dates: "December 19th - 23rd, 2023",
+      location: "Bhubaneswar, India",
+      description:
+        "Developed A collaborative coding web platform that enables Multiuser collboration on a coding project in real-time with features of group chat, to-do lists.",
+      image:
+        "https://i.ibb.co/fM326Bv/logo.png",
+      mlh: "https://nexuslink01v.netlify.app/",
+      links: [],
+    },
+  ],
+} as const;

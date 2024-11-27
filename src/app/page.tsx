883881b4999eb80950f5f340fe47673293ccaf4e
StarKnightt <prasenjitt4e@gmail.com>
2024-11-27 14:15:34 +0530

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -110,7 +110,9 @@ export default function Page() {
                   Check out my latest work
                 </h2>
                 <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                I&apos;ve worked on many projects, from small websites to big web apps. Here are a few I'm especially proud of.
+                I&apos;ve worked on a variety of projects, from simple
+                  websites to complex web applications. Here are a few of my
+                  favorites
                 </p>
               </div>
             </div>

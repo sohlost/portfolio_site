092162a0fd0d28f7dec7d54ccdb27cc186b73e38
Anshul Soni <anshulsoni2022@gmail.com>
2024-12-01 15:30:17 +0530

    clarity analytic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,6 +58,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+ (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "p73rco1nfp");
+            `,
+          }}
+        />
+
+
+      </head>
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased max-w-2xl mx-auto py-12 sm:py-24 px-6",

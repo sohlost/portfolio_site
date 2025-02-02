7bc73cb90fb494e353f53335dd5963d0e3913fba
StarKnightt <prasenjitt4e@gmail.com>
2025-02-02 20:40:45 +0530

    updated RSS

--- src/app/rss.xml/route.ts
+++ src/app/rss.xml/route.ts
@@ -8,21 +8,52 @@ export async function GET() {
   });
 
   const rss = `<?xml version="1.0" encoding="UTF-8"?>
-    <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+    <rss version="2.0" 
+      xmlns:atom="http://www.w3.org/2005/Atom"
+      xmlns:dc="http://purl.org/dc/elements/1.1/"
+      xmlns:content="http://purl.org/rss/1.0/modules/content/"
+      xmlns:webfeeds="http://webfeeds.org/rss/1.0"
+    >
       <channel>
         <title>${DATA.name}'s Blog</title>
-        <description>Latest blog posts from ${DATA.name}</description>
+        <description>${DATA.description}</description>
         <link>${DATA.url}</link>
+        <language>en</language>
+        <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
+        <generator>Next.js</generator>
         <atom:link href="${DATA.url}/rss.xml" rel="self" type="application/rss+xml"/>
+        <webfeeds:cover image="${DATA.url}/og-image.png" />
+        <webfeeds:icon>${DATA.url}/favicon.ico</webfeeds:icon>
+        <webfeeds:accentColor>2563EB</webfeeds:accentColor>
+        <webfeeds:related layout="card" target="browser"/>
+        <image>
+          <url>${DATA.url}/og-image.png</url>
+          <title>${DATA.name}'s Blog</title>
+          <link>${DATA.url}</link>
+        </image>
+        <copyright>All rights reserved ${new Date().getFullYear()}, ${DATA.name}</copyright>
         ${sortedPosts
           .map(
             (post) => `
             <item>
               <title>${post.metadata.title}</title>
-              <description>${post.metadata.summary}</description>
-              <pubDate>${new Date(post.metadata.publishedAt).toUTCString()}</pubDate>
               <link>${DATA.url}/blog/${post.slug}</link>
               <guid isPermaLink="true">${DATA.url}/blog/${post.slug}</guid>
+              <pubDate>${new Date(post.metadata.publishedAt).toUTCString()}</pubDate>
+              <description><![CDATA[${post.metadata.summary}]]></description>
+              <content:encoded><![CDATA[${post.source}]]></content:encoded>
+              <dc:creator>${DATA.name}</dc:creator>
+              ${post.metadata.tags ? 
+                post.metadata.tags.map(tag => `<category>${tag}</category>`).join('') 
+                : ''}
+              ${post.metadata.image ? 
+                `<media:content 
+                  url="${DATA.url}${post.metadata.image}" 
+                  medium="image" 
+                  type="image/jpeg"
+                />` 
+                : ''}
+              <author>${DATA.contact.email} (${DATA.name})</author>
             </item>
           `
           )

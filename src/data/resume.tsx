80ec1fa6df7176e10265026cbce008516a20140b
StarKnightt <prasenjitt4e@gmail.com>
2025-03-03 00:44:44 +0530

    updated

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -244,7 +244,7 @@ export const DATA = {
       links: [
         {
           type: "Website",
-          href: "https://resume-builder-ten-opal.vercel.app/",
+          href: "https://builddresume.vercel.app/",
           icon: <Icons.globe className="size-3" />,
         },
         {

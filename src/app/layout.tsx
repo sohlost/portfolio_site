e08e4124eb3e432b4971bebe8a39711bdba7ef32
StarKnightt <prasenjitt4e@gmail.com>
2025-05-03 19:49:03 +0530

    removed posthog, becaue it was unncessary

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,6 @@ import { PageBackground } from "@/components/page-background";
 import { Analytics } from "@vercel/analytics/react";
 import { GoogleAnalytics } from '@next/third-parties/google'
 import { SpeedInsights } from "@vercel/speed-insights/react";
-import { PostHogProvider } from "@/app/providers";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -124,39 +123,37 @@ export default function RootLayout({
           <PageBackground />
         </div>
 
-        <PostHogProvider>
-          {/* Main content */}
-          <div className="relative z-10 max-w-2xl mx-auto py-12 sm:py-24 px-6">
-            <GoogleAnalytics gaId="G-XVF0SFD4GW" />
-            <Script
-              id="microsoft-clarity"
-              strategy="afterInteractive"
-            >
-              {`
-                (function(c,l,a,r,i,t,y){
-                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-                })(window, document, "clarity", "script", "p73rco1nfp");
-              `}
-            </Script>
-            <JsonLd />
-            <ScrollProgress />
-            <ThemeProvider
-              attribute="class"
-              defaultTheme="system"
-              enableSystem
-              disableTransitionOnChange
-            >
-              <TooltipProvider delayDuration={0}>
-                {children}
-                <Analytics />
-                <SpeedInsights />
-                <Navbar />
-              </TooltipProvider>
-            </ThemeProvider>
-          </div>
-        </PostHogProvider>
+        {/* Main content */}
+        <div className="relative z-10 max-w-2xl mx-auto py-12 sm:py-24 px-6">
+          <GoogleAnalytics gaId="G-XVF0SFD4GW" />
+          <Script
+            id="microsoft-clarity"
+            strategy="afterInteractive"
+          >
+            {`
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "p73rco1nfp");
+            `}
+          </Script>
+          <JsonLd />
+          <ScrollProgress />
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <TooltipProvider delayDuration={0}>
+              {children}
+              <Analytics />
+              <SpeedInsights />
+              <Navbar />
+            </TooltipProvider>
+          </ThemeProvider>
+        </div>
       </body>
     </html>
   );
